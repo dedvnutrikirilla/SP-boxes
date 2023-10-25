8b3af5bb1a72b10f3d8405d28168c790ac469be5
dedvnutrikirilla <93326037+dedvnutrikirilla@users.noreply.github.com>
2023-10-25 23:48:39 +0300

    Update p_box.c

--- src/p_box.c
+++ src/p_box.c
@@ -1,7 +1,7 @@
 #include"../inc/spboxing.h"
 
 // Used for better understanding of bit operations, better be deleted
-/*const uint8_t p_box2[8] = {
+/*const uint8_t p_box[8] = {
     0b00100000,
     0b00010000,
     0b10000000,
